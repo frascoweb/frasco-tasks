88cababcb1a9a3d7415ece12aeb06603a950d597
maximebf <maxime.bouroumeau@gmail.com>
2014-10-23 00:39:01 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup
 
 
-def desc():
-    with open("README.md") as f:
-        return f.read()
-
-def reqs():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
 setup(
     name='frasco-tasks',
     version='0.1',
@@ -17,8 +9,11 @@ setup(
     author='Maxime Bouroumeau-Fuseau',
     author_email='maxime.bouroumeau@gmail.com',
     description="Celery integration for Frasco",
-    long_description=desc(),
     py_modules=['frasco_tasks'],
+    zip_safe=False,
     platforms='any',
-    install_requires=reqs() + ['frasco']
+    install_requires=[
+        # 'frasco',
+        'celery-with-redis>=3.0'
+    ]
 )
